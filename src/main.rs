f09d86dc24b5a30c78f199272e00ad00793ba529
osama530 <osamaqamar93@gmail.com>
2020-06-23 11:57:20 +0500

    update with wury string

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,21 @@
 	
      #[macro_use]
      extern crate rocket;
+ //************* with dynamic perameteres ****************//    
+    //  #[get("/example/<value>")]
+    //  fn add(value: i32) -> String {
+    //      (value + 20).to_string()
+    //  }
      
-     #[get("/example/<value>")]
+    //  fn main() {
+    //      rocket::ignite().mount("/", routes![add]).launch();
+    //  }
+
+
+// ****************With qury parameters*************** //
+     #[get("/example?cons=20&<value>")]
      fn add(value: i32) -> String {
-         (value + 20).to_string()
+         (20 + value).to_string()
      }
      
      fn main() {
